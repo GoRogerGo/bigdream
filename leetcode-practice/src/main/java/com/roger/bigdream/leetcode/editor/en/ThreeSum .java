3d54b15418300be3c62724b59bc77d43b30163d8
陈桂阳 <chenguiyang@meituan.com>
2020-06-17 16:17:07 +0800

    leetcode no15

--- leetcode-practice/src/main/java/com/roger/bigdream/leetcode/editor/en/ThreeSum .java
+++ leetcode-practice/src/main/java/com/roger/bigdream/leetcode/editor/en/ThreeSum .java
@@ -22,17 +22,51 @@
 
 package com.roger.bigdream.leetcode.editor.en;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class ThreeSum {
     public static void main(String[] args) {
         Solution solution = new ThreeSum().new Solution();
+        solution.threeSum(new int[]{-1, 0, -1, 1, 2, -1, -4});
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public List<List<Integer>> threeSum(int[] nums) {
-            return null;
+            List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+            // 先排序
+            Arrays.sort(nums);
+
+            // 转换为2sum问题
+            for (int i = 0; i < nums.length - 2; i++) {
+                if (nums[i] > 0) break;
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+                int target = 0 - nums[i];
+                int left = i + 1;
+                int right = nums.length - 1;
+                while (left < right) {
+                    if (nums[left] + nums[right] == target) {
+                        result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        left++;
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            left++;
+                        }
+                        right--;
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            right--;
+                        }
+                    } else if (nums[left] + nums[right] < target) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+            return result;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
