bb7c528f8eaed2fdd93934eec27f71229ca28386
陈桂阳 <chenguiyang@meituan.com>
2020-07-21 22:24:09 +0800

    leetcode no47

--- leetcode-practice/src/main/java/com/roger/bigdream/leetcode/editor/en/PermutationsIi .java
+++ leetcode-practice/src/main/java/com/roger/bigdream/leetcode/editor/en/PermutationsIi .java
@@ -18,22 +18,91 @@
 
 package com.roger.bigdream.leetcode.editor.en;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class PermutationsIi {
     public static void main(String[] args) {
         Solution solution = new PermutationsIi().new Solution();
+//        solution.permuteUnique(new int[]{1, 2, 3});
+        solution.permuteUnique(new int[]{1, 1, 2});
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        /**
+         * Runtime:5 ms, faster than 33.27% of Java online submissions.
+         * Memory Usage:44.2 MB, less than 5.56% of Java online submissions.
+         *
+         * @param nums
+         * @return
+         */
         public List<List<Integer>> permuteUnique(int[] nums) {
             List<List<Integer>> res = new ArrayList<>();
-
+            if (nums.length < 1) return res;
+            Arrays.sort(nums);
+            dfs(res, nums, new ArrayList<>(), new boolean[nums.length]);
             return res;
+        }
+
+        private void dfs(List<List<Integer>> res, int[] nums, List<Integer> cur, boolean[] visited) {
+            if (cur.size() == nums.length) {
+                res.add(new ArrayList<>(cur));
+                return;
+            }
+            for (int i = 0; i < nums.length; i++) {
+                if (visited[i]) continue;
+                if (i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) continue;
+                cur.add(nums[i]);
+                visited[i] = true;
+                dfs(res, nums, cur, visited);
+                cur.remove(cur.size() - 1);
+                visited[i] = false;
+            }
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
+
+    private List<List<Integer>> myApproach(int[] nums) {
+        Set<List<Integer>> res = new HashSet<>();
+
+        List<Boolean> used = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            used.add(false);
+        }
+
+        List<Integer> path = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            path.add(-1);
+        }
+
+        dfs(0, res, nums, used, path);
+
+        return new ArrayList<>(res);
+    }
+
+    /**
+     * Runtime:37 ms, faster than 14.75% of Java online submissions.
+     * Memory Usage:40.1 MB, less than 63.44% of Java online submissions.
+     *
+     * @param curPos
+     * @param res
+     * @param nums
+     * @param used
+     * @param path
+     */
+    private void dfs(int curPos, Set<List<Integer>> res, int[] nums, List<Boolean> used, List<Integer> path) {
+        if (curPos == nums.length) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!used.get(i)) {
+                used.set(i, true);
+                path.set(i, nums[curPos]);
+                dfs(curPos + 1, res, nums, used, path);
+                used.set(i, false);
+            }
+        }
+    }
 }
