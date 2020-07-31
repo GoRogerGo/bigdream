9396ac75c4f038cacb9c763458cfc36466f827c0
陈桂阳 <chenguiyang@meituan.com>
2020-07-31 19:59:20 +0800

    leetcode no36

--- leetcode-practice/src/main/java/com/roger/bigdream/leetcode/editor/en/ValidSudoku .java
+++ leetcode-practice/src/main/java/com/roger/bigdream/leetcode/editor/en/ValidSudoku .java
@@ -70,16 +70,89 @@
 
 package com.roger.bigdream.leetcode.editor.en;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class ValidSudoku {
     public static void main(String[] args) {
         Solution solution = new ValidSudoku().new Solution();
+        System.out.println(solution.isValidSudoku(new char[][]{
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}}));
+        System.out.println(solution.isValidSudoku(new char[][]{
+                {'8', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}}));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+
+        /**
+         * 一个有效的数独（部分已被填充）不一定是可解的。
+         * 数字 1-9 在每一行只能出现一次。
+         * 数字 1-9 在每一列只能出现一次。
+         * 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。
+         *
+         * Runtime:3 ms, faster than 69.87% of Java online submissions.
+         * Memory Usage:39.7 MB, less than 30.87% of Java online submissions.
+         *
+         * @param board
+         * @return
+         */
         public boolean isValidSudoku(char[][] board) {
-            return false;
+            Set<Character> set = new HashSet<>();
+            for (char[] chars : board) {
+                for (char c : chars) {
+                    if (c != '.' && !set.add(c)) {
+                        return false;
+                    }
+                }
+                set.clear();
+            }
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board.length; j++) {
+                    char c = board[j][i];// i行j列
+                    if (c != '.' && !set.add(c)) {
+                        return false;
+                    }
+                }
+                set.clear();
+            }
+
+            int[] array = new int[]{0, 3, 6};
+            int count = 0;
+            for (int k = 0; k < array.length; k++) {
+                for (int i = 0; i < board.length; i++) {
+                    for (int j = array[k]; j < array[k] + 3; j++) {
+                        char c = board[i][j];
+                        count++;
+                        if (c != '.' && !set.add(c)) {
+                            return false;
+                        }
+                    }
+                    if (count == 9) {
+                        count = 0;
+                        set.clear();
+                    }
+                }
+            }
+            return true;
         }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
