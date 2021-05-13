739f8fd81199b003d6597db78cb5a30d96e9dd02
陈桂阳 <chenguiyang@meituan.com>
2021-05-13 23:49:40 +0800

    leetcode 206 & 234

---
+++ leetcode-practice/src/main/java/com/roger/bigdream/leetcode/editor/en/ReverseLinkedList.java
@@ -0,0 +1,93 @@
+package com.roger.bigdream.leetcode.editor.en;
+
+//Given the head of a singly linked list, reverse the list, and return the rever
+//sed list. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [1,2,3,4,5]
+//Output: [5,4,3,2,1]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = [1,2]
+//Output: [2,1]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: head = []
+//Output: []
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is the range [0, 5000]. 
+// -5000 <= Node.val <= 5000 
+// 
+//
+// 
+// Follow up: A linked list can be reversed either iteratively or recursively. C
+//ould you implement both? 
+// Related Topics Linked List 
+// 👍 6969 👎 132
+
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        Solution solution = new ReverseLinkedList().new Solution();
+
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            if (head == null || head.next == null) return head;
+
+            ListNode reverseHead = null;
+            while (head != null) {
+                ListNode second = head.next;
+                head.next = reverseHead;
+                reverseHead = head;
+                head = second;
+            }
+            return reverseHead;
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
