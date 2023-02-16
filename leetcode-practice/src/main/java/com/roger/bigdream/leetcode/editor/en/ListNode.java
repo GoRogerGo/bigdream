e39997b90cd5809aca2251cbb9ab74d940de0c53
chenguiyang <peerless_hero@sina.cn>
2023-02-16 20:42:27 +0800

    rest

---
+++ leetcode-practice/src/main/java/com/roger/bigdream/leetcode/editor/en/ListNode.java
@@ -0,0 +1,25 @@
+package com.roger.bigdream.leetcode.editor.en;
+
+/**
+ * @ClassName ListNode
+ * @Description
+ * @Author chenguiyang
+ * @Date 2023/2/16 下午8:40
+ * @Version 1.0
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
