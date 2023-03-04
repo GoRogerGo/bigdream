ffc2db1c0f19eaa9dcaaba5a92d4693e447effd9
chenguiyang <peerless_hero@sina.cn>
2023-03-04 22:29:26 +0800

    leetcode 23 一次写对:性能不好

--- leetcode-practice/src/main/java/com/roger/bigdream/leetcode/editor/en/_23MergeKSortedLists.java
+++ leetcode-practice/src/main/java/com/roger/bigdream/leetcode/editor/en/_23MergeKSortedLists.java
@@ -49,7 +49,7 @@ package com.roger.bigdream.leetcode.editor.en;
 //t 
 // 👍 15636 👎 584
 
-public class _23MergeKSortedLists{
+public class _23MergeKSortedLists {
 
     public static void main(String[] args) {
         Solution solution = new _23MergeKSortedLists().new Solution();
@@ -57,21 +57,62 @@ public class _23MergeKSortedLists{
     }
 
 //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        return null;
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        /**
+         * 下午10:28	info
+         * 解答成功:
+         * 执行耗时:105 ms,击败了19.49% 的Java用户
+         * 内存消耗:45 MB,击败了18.02% 的Java用户
+         * <p>
+         * 一次写对，但性能不好。。。 2023年03月04日22:28:48
+         *
+         * @param lists
+         * @return
+         */
+        public ListNode mergeKLists(ListNode[] lists) {
+            if (lists.length == 0) return null;
+            else if (lists.length == 1) return lists[0];
+            else {
+                ListNode result = lists[0];
+                for (int index = 1; index < lists.length; index++) {
+                    result = mergeTwoLists(result, lists[index]);
+                }
+                return result;
+            }
+        }
+
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            if (list1 == null) return list2;
+            if (list2 == null) return list1;
+            ListNode dummyHead = new ListNode(0);
+            ListNode node = dummyHead;
+            while (list1 != null && list2 != null) {
+                if (list1.val < list2.val) {
+                    node.next = list1;
+                    list1 = list1.next;
+                } else {
+                    node.next = list2;
+                    list2 = list2.next;
+                }
+                node = node.next;
+            }
+            if (list1 != null) node.next = list1;
+            if (list2 != null) node.next = list2;
+            return dummyHead.next;
+
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 
