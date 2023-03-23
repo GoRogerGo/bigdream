374ee70f9e674da64cd254ac7230bb6f2420b015
chenguiyang <chenguiyang@chenguiyangdeMacBook-Pro-2.local>
2023-03-24 01:12:45 +0800

    leetcode 986

--- leetcode-practice/src/main/java/com/roger/bigdream/leetcode/editor/en/_986IntervalListIntersections.java
+++ leetcode-practice/src/main/java/com/roger/bigdream/leetcode/editor/en/_986IntervalListIntersections.java
@@ -16,8 +16,8 @@ package com.roger.bigdream.leetcode.editor.en;
 // Example 1: 
 //
 // 
-//Input: firstList = [[0,2],[5,10],[13,23],[24,25]], secondList = [[1,5],[8,12],
-//[15,24],[25,26]]
+//Input: firstList = [[0,2],[5,10],[13,23],[24,25]],
+//      secondList = [[1,5],[8,12],[15,24],[25,26]]
 //Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
 // 
 //
@@ -42,17 +42,121 @@ package com.roger.bigdream.leetcode.editor.en;
 // Related Topics Array Two Pointers 
 // 👍 5015 👎 98
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class _986IntervalListIntersections {
 
     public static void main(String[] args) {
         Solution solution = new _986IntervalListIntersections().new Solution();
-
+        System.out.println(solution.intervalIntersection(new int[][]{{0, 2}, {5, 10}, {13, 23}, {24, 25}}, new int[][]{{1, 5}, {8, 12}, {15, 24}, {25, 26}}));
+        System.out.println(solution.intervalIntersection(new int[][]{{5, 10}}, new int[][]{{5, 6}}));
+        System.out.println(solution.intervalIntersection(new int[][]{{3, 5}, {9, 20}}, new int[][]{{4, 5}, {7, 10}, {11, 12}, {14, 15}, {16, 20}}));
+        System.out.println(solution.intervalIntersection(new int[][]{{4, 6}, {7, 8}, {10, 17}}, new int[][]{{5, 10}}));
+        System.out.println(solution.intervalIntersection(new int[][]{{0, 4}, {7, 8}, {12, 19}}, new int[][]{{0, 10}, {14, 15}, {18, 20}}));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        private int[][] defaultRes = new int[][]{};
+
+        /**
+         * 解答成功: 执行耗时:2 ms,击败了99.17% 的Java用户 内存消耗:43.3 MB,击败了43.21% 的Java用户
+         * 2023年03月24日01:12:06
+         * 别人的做法，时间太晚了，后面再研读吧
+         *
+         * @param firstList
+         * @param secondList
+         * @return
+         */
         public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-            return new int[][]{{}};
+            List<int[]> result = new LinkedList<>();
+            getInterval(firstList, secondList, 0, 0, result);
+            return result.toArray(new int[result.size()][]);
+        }
+
+        public List<int[]> getInterval(int[][] firstList, int[][] secondList, int pointerA, int pointerB, List<int[]> result) {
+            if (pointerA == firstList.length || pointerB == secondList.length) {
+                return result;
+            }
+            int[] a = firstList[pointerA];
+            int[] b = secondList[pointerB];
+            int min = Math.max(a[0], b[0]);
+            int max = Math.min(a[1], b[1]);
+            if (min <= max) {
+                result.add(new int[]{min, max});
+                if (a[1] == max) {
+                    return getInterval(firstList, secondList, pointerA + 1, pointerB, result);
+                }
+                return getInterval(firstList, secondList, pointerA, pointerB + 1, result);
+            }
+            if (a[0] == min) {
+                return getInterval(firstList, secondList, pointerA, pointerB + 1, result);
+            }
+            return getInterval(firstList, secondList, pointerA + 1, pointerB, result);
+        }
+
+        /**
+         * 解答成功: 执行耗时:21 ms,击败了5.66% 的Java用户 内存消耗:43.7 MB,击败了22.53% 的Java用户
+         * 2023年03月24日01:05:06
+         * 花了很久，才把逻辑条件写对：new Pair(Math.max(firstL, secondL), Math.min(firstR, secondR))
+         *
+         * @param firstList
+         * @param secondList
+         * @return
+         */
+        public int[][] intervalIntersection_myself(int[][] firstList, int[][] secondList) {
+            if (firstList.length == 0 || secondList.length == 0) return defaultRes;
+            int firstLen = firstList.length;
+            int secondLen = secondList.length;
+
+            if (firstLen > secondLen) return intervalIntersection_myself(secondList, firstList);
+            // 特殊情况快速返回
+            int firstMin = firstList[0][0];
+            int firstMax = firstList[firstLen - 1][1];
+            int secondMin = secondList[0][0];
+            int secondMax = secondList[secondLen - 1][1];
+            if (firstMax < secondMin || firstMin > secondMax) return defaultRes;
+
+            List<Pair> pairs = new ArrayList<>();
+            for (int i = secondLen; i > 0; i--) {
+                int secondL = secondList[i - 1][0];
+                int secondR = secondList[i - 1][1];
+                for (int j = firstLen; j > 0; j--) {
+                    int firstL = firstList[j - 1][0];
+                    int firstR = firstList[j - 1][1];
+                    // 快速返回
+                    if (firstL > secondR || firstR < secondL) {
+                        continue;
+                    }
+                    pairs.add(new Pair(Math.max(firstL, secondL), Math.min(firstR, secondR)));
+                }
+            }
+            int[][] res = new int[pairs.size()][];
+            for (int i = 0; i < pairs.size(); i++) {
+                int index = pairs.size() - 1 - i;
+                res[i] = new int[]{pairs.get(index).getFirst(), pairs.get(index).getSecond()};
+            }
+            return res;
+        }
+
+        class Pair {
+            private int first;
+            private int second;
+
+            public int getFirst() {
+                return first;
+            }
+
+            public int getSecond() {
+                return second;
+            }
+
+            public Pair(int first, int second) {
+                this.first = first;
+                this.second = second;
+            }
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
