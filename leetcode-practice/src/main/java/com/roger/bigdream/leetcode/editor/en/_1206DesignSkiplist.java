066a6f27a6c0379ed889270745db81c6650ed771
chenguiyang <chenguiyang@chenguiyangdeMacBook-Pro-2.local>
2023-07-24 09:53:01 +0800

    leetcode 61 & 1206

---
+++ leetcode-practice/src/main/java/com/roger/bigdream/leetcode/editor/en/_1206DesignSkiplist.java
@@ -0,0 +1,180 @@
+package com.roger.bigdream.leetcode.editor.en;
+//Design a Skiplist without using any built-in libraries. 
+//
+// A skiplist is a data structure that takes O(log(n)) time to add, erase and se
+//arch. Comparing with treap and red-black tree which has the same function and pe
+//rformance, the code length of Skiplist can be comparatively short and the idea b
+//ehind Skiplists is just simple linked lists. 
+//
+// For example, we have a Skiplist containing [30,40,50,60,70,90] and we want to
+// add 80 and 45 into it. The Skiplist works this way: 
+//
+// 
+//Artyom Kalinin [CC BY-SA 3.0], via Wikimedia Commons 
+//
+// You can see there are many layers in the Skiplist. Each layer is a sorted lin
+//ked list. With the help of the top layers, add, erase and search can be faster t
+//han O(n). It can be proven that the average time complexity for each operation i
+//s O(log(n)) and space complexity is O(n). 
+//
+// See more about Skiplist: https://en.wikipedia.org/wiki/Skip_list 
+//
+// Implement the Skiplist class: 
+//
+// 
+// Skiplist() Initializes the object of the skiplist. 
+// bool search(int target) Returns true if the integer target exists in the Skip
+//list or false otherwise. 
+// void add(int num) Inserts the value num into the SkipList. 
+// bool erase(int num) Removes the value num from the Skiplist and returns true.
+// If num does not exist in the Skiplist, do nothing and return false. If there ex
+//ist multiple num values, removing any one of them is fine. 
+// 
+//
+// Note that duplicates may exist in the Skiplist, your code needs to handle thi
+//s situation. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input
+//["Skiplist", "add", "add", "add", "search", "add", "search", "erase", "erase",
+// "search"]
+//[[], [1], [2], [3], [0], [4], [1], [0], [1], [1]]
+//Output
+//[null, null, null, null, false, null, true, false, true, false]
+//
+//Explanation
+//Skiplist skiplist = new Skiplist();
+//skiplist.add(1);
+//skiplist.add(2);
+//skiplist.add(3);
+//skiplist.search(0); // return False
+//skiplist.add(4);
+//skiplist.search(1); // return True
+//skiplist.erase(0);  // return False, 0 is not in skiplist.
+//skiplist.erase(1);  // return True
+//skiplist.search(1); // return False, 1 has already been erased. 
+//
+// 
+// Constraints: 
+//
+// 
+// 0 <= num, target <= 2 * 104 
+// At most 5 * 104 calls will be made to search, add, and erase. 
+// 
+// Related Topics Linked List Design 
+// 👍 594 👎 68
+
+import java.util.Random;
+import java.util.Stack;
+
+public class _1206DesignSkiplist {
+
+    public static void main(String[] args) {
+
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Skiplist {
+
+        public Node head;
+
+        public Skiplist() {
+            head = new Node(-1);
+        }
+
+        /**
+         * 解答成功: 执行耗时:445 ms,击败了18.25% 的Java用户 内存消耗:51.6 MB,击败了46.72% 的Java用户
+         * 2023年07月24日09:10:40
+         * 第二次了，学习了算法知识，看了花花的讲解，其实没那么难
+         *
+         * @param target
+         * @return
+         */
+        public boolean search(int target) {
+            Node node = head;
+            while (node != null) {
+                while (node.next != null && node.next.val < target) {
+                    node = node.next;
+                }
+                if (node.next != null && node.next.val == target) return true;
+                node = node.down;
+            }
+            return false;
+        }
+
+        public void add(int num) {
+            // 将每一层小于num的最大元素存储下来，后续在这些元素后插入新结点
+            Stack<Node> nodes = new Stack<>();
+            Node node = head;
+            while (node != null) {
+                while (node.next != null && node.next.val < num) {
+                    node = node.next;
+                }
+                nodes.push(node);
+                node = node.down;
+            }
+
+            boolean insert = true;
+            Node down = null;
+            while (!nodes.empty() && insert) {
+                Node prev = nodes.pop(); // 从下往上
+                prev.next = new Node(num, prev.next, down);
+                down = prev.next; // 画龙点睛之笔，对于上一层来说，down指向了新插入的结点！
+                insert = (new Random(2).nextInt() == 0);
+            }
+
+            // 新增一层
+            if (insert) {
+                head = new Node(-1, null, head);
+            }
+        }
+
+        public boolean erase(int num) {
+            boolean flag = false;
+            Node node = head;
+            while (node != null) {
+                while (node.next != null && node.next.val < num) {
+                    node = node.next;
+                }
+                if (node.next != null && node.next.val == num) {
+                    flag = true;
+                    node.next = node.next.next;
+                }
+                node = node.down;
+            }
+            return flag;
+        }
+    }
+
+    class Node {
+        public int val;
+        public Node next;
+        public Node down;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.down = null;
+        }
+
+        public Node(int val, Node next, Node down) {
+            this.val = val;
+            this.next = next;
+            this.down = down;
+        }
+    }
+
+/**
+ * Your Skiplist object will be instantiated and called as such:
+ * Skiplist obj = new Skiplist();
+ * boolean param_1 = obj.search(target);
+ * obj.add(num);
+ * boolean param_3 = obj.erase(num);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
---
+++ leetcode-practice/src/main/java/com/roger/bigdream/leetcode/editor/en/_61RotateList.java
@@ -0,0 +1,101 @@
+package com.roger.bigdream.leetcode.editor.en;
+//Given the head of a linked list, rotate the list to the right by k places. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: head = [1,2,3,4,5], k = 2
+//Output: [4,5,1,2,3]
+// 
+//
+// Example 2: 
+//
+// 
+//Input: head = [0,1,2], k = 4
+//Output: [2,0,1]
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// The number of nodes in the list is in the range [0, 500]. 
+// -100 <= Node.val <= 100 
+// 0 <= k <= 2 * 109 
+// 
+// Related Topics Linked List Two Pointers 
+// 👍 8285 👎 1392
+
+public class _61RotateList {
+
+    public static void main(String[] args) {
+        Solution solution = new _61RotateList().new Solution();
+        System.out.println(solution.rotateRight(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))), 2));
+        System.out.println(solution.rotateRight(new ListNode(0, new ListNode(1, new ListNode(2))), 4));
+
+    }
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        /**
+         * 2023年07月24日09:23:28
+         * 解答成功: 执行耗时:0 ms,击败了100.00% 的Java用户 内存消耗:42 MB,击败了8.91% 的Java用户
+         *
+         * <p>
+         * 看了花花的伪代码，最后还是没有写对，有3处改动
+         * reach to the tail node T and count how many nodes in the list
+         * get P by moving n-k-1 steps from H
+         * N = P.next
+         * P.next = null
+         * T.next = H
+         * return N
+         *
+         * @param head
+         * @param k
+         * @return
+         */
+        public ListNode rotateRight(ListNode head, int k) {
+            if (head == null || head.next == null || k == 0) return head;
+            int count = 1;
+
+            ListNode node = head;
+            while (node.next != null) {
+                count++;
+                node = node.next;
+            }
+//            ListNode tail = node; 改动3：这行代码很不优雅，其实node就是tail
+
+//            int kk = k % count; 改动1：直接赋值，而不是新定义变量；同时，如果k等于0，直接返回head
+            k %= count;
+            if (k == 0) return head;
+
+
+            ListNode P = head;
+            for (int i = 0; i < count - k - 1; i++) {
+//                for (int i = 0; i < kk; i++) { 改动2：moving数不对，不是k，是count - k -1
+                P = P.next;
+            }
+            ListNode N = P.next;
+            P.next = null;
+//            tail.next = head; 接改动3：这里用node
+            node.next = head;
+            return N;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
